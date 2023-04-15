4fd80e6b92d9462d0c919b301757351191c1e3af
Jaunyoung <jjy889@naver.com>
2023-04-15 15:40:11 +0900

    java tutorial: previous study

---
+++ src/MyProfile.java
@@ -0,0 +1,7 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        System.out.print("정재영");
+        System.out.print("jjy889@naver.com");
+        System.out.print("male");
+    }
+}
---
+++ src/tutorials/jae/Hello.java
@@ -0,0 +1,7 @@
+package tutorials.jae;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("정재영, 남성, jjy889@naver.com");
+    }
+}
